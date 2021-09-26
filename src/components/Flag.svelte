526926993c12f3089f23287ea6e6da1cb31f55ef
Christian Bachhelm <cbachhelm@gmail.com>
2021-09-26 11:57:47 +0200

    Add basic Flag component.

---
+++ src/components/Flag.svelte
@@ -0,0 +1,7 @@
+<script>
+	export let countryCode;
+</script>
+
+<img alt="flag-{countryCode}" src="static/flags/{countryCode}.svg" />
+
+<style></style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+	import Flag from '../components/Flag.svelte';
+</script>
+
+<h1>Welcome to Covid19 Track</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<Flag countryCode="de"/>

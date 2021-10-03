f183a52cbae9ac1bebab8a836bf450018752cf8f
Christian Bachhelm <cbachhelm@gmail.com>
2021-10-03 15:45:46 +0200

    Add custom colors to tailwind config.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,6 +8,23 @@ module.exports = {
 			white: colors.white,
 			gray: colors.warmGray,
 			'svelte-prime': '#ff5030',
+			blue: {
+				dark: {
+					'100': '#03045E',
+					'200': '#023E8A',
+					'300': '#0077B6',
+				},
+				base: {
+					dark: '#0096C7',
+					main: '#00B4D8',
+					light: '#48CAE4',
+				},
+				light: {
+					'100': '#90E0Ef',
+					'200': '#ADE8F4',
+					'300': '#CAF0F8',
+				}
+			},
 		},
 	},
 	variants: {

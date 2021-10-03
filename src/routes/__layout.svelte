bcb564e99319e4be0d48970b09f1075352dd8a8b
Christian Bachhelm <cbachhelm@gmail.com>
2021-10-03 13:04:00 +0200

    Clean up global layout.

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,31 +1,9 @@
 <script>
 	import '../tailwind.css'
-	import countriesJson from '/static/data/countries.json';
-	import { goto } from '$app/navigation';
-	import Typeahead from "svelte-typeahead";
-
-	const extract = (item) => item.name;
-
-	let country = null;
-
-	async function update(detail) {
-		country = { detail };
-		await goto(`/${detail.original.countryCodeAlpha3.toLowerCase()}`);
-	}
-
-	function clear() {
-		country = null;
-	}
+	import Navbar from '../components/Navbar.svelte';
 </script>
 
-<Typeahead
-	class="ml-2"
-	placeholder={`Search for country name`}
-	hideLabel
-	data={countriesJson}
-	{extract}
-	on:select={(e) => update(e.detail)}
-	on:clear={() => clear() }
-/>
-
-<slot />
+<main>
+	<Navbar />
+	<slot />
+</main>

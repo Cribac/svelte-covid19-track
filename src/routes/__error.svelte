cc354f70e3175fb25d7ea02ed57dc365336eb1e8
Christian Bachhelm <cbachhelm@tuta.io>
2021-10-19 20:29:02 +0200

    Be a bit more sophisticated in displaying errors.

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -10,13 +10,23 @@
 	export let status;
 
 	const messages = {
+		'unknown': 'An unknown error occurred.',
 		404: 'Country data is not available',
 	};
 
+	/**
+	 *
+	 * @param {number} status
+	 * @returns {string}
+	 */
+	const getError = (status) => {
+		const errCode = Object.keys(messages).find(key => key === status.toString());
+		return messages[errCode] || messages['unknown'];
+	}
 </script>
 
 <div class="p-3">
 	<div>
-		Sorry, something went wrong: {messages[status]}
+		Sorry, something went wrong: {getError(status)}
 	</div>
 </div>

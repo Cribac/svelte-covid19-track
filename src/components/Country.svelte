d68ff5f5e49117c8ddd6930238e70466199290a2
Christian Bachhelm <cbachhelm@tuta.io>
2022-01-25 23:23:38 +0100

    Wrap country info on mobile.

--- src/components/Country.svelte
+++ src/components/Country.svelte
@@ -10,9 +10,9 @@
 	const formattedDate = DateTime.fromFormat(covidData.date, 'yyyy-MM-d').toFormat('yyyy LLL dd');
 </script>
 
-<div class="flex">
+<div class="flex flex-wrap">
 	<Flag countryCode={countryCode} />
-	<div class="flex flex-col ml-3">
+	<div class="flex flex-col">
 		<div class="text-base text-blue-dark-100">{countryName}</div>
 		<div class="text-sm text-blue-dark-100 mt-1">Latest data at: {formattedDate}</div>
 		<div class="text-sm text-blue-dark-100 mt-1">Confirmed Cases: {covidData.confirmed}</div>

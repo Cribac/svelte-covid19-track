8c8220fab039c2d0b3ddc3a2d8c27893a53271e0
Christian Bachhelm <cbachhelm@tuta.io>
2022-02-02 22:48:04 +0100

    Remove obsolete config.kit.target configuration.

--- svelte.config.js
+++ svelte.config.js
@@ -7,8 +7,6 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
 		vite: {
 			optimizeDeps: {
 				include: ["fuzzy"], // needed for typeahead fuzzy search

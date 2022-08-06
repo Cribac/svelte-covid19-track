2567e15ff3d5b5b9f0dda34f8574a6588edeed51
Christian Bachhelm <cbachhelm@tuta.io>
2022-08-06 17:31:17 +0200

    Replace http get() with uppercase GET(). @see https://github.com/sveltejs/kit/discussions/5359

--- src/routes/api/v1/covidData.ts
+++ src/routes/api/v1/covidData.ts
@@ -1,4 +1,4 @@
-export async function get():Promise<CovidApiJson> {
+export async function GET():Promise<CovidApiJson> {
 	const request: Response = await fetch('https://pomber.github.io/covid19/timeseries.json');
 	if (request.ok) {
 		const result: ApiResult = await request.json();

89ccf517281c28649a78a1aff75bc407e9bb1fd0
Christian Bachhelm <cbachhelm@tuta.io>
2022-08-13 13:12:12 +0200

    Enhance the error route with the country name.

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -1,17 +1,20 @@
 <script context="module">
-	export function load({ status }) {
+	import { CountryService } from '../utils/countryService';
+
+	export function load({ status, params }) {
 		return {
-			props: { status },
+			props: { status, params },
 		};
 	}
 </script>
 
 <script>
 	export let status;
+	export let params;
 
 	const messages = {
 		'unknown': 'An unknown error occurred.',
-		404: 'Country data is not available',
+		404: 'No data available for Country: ',
 	};
 
 	/**
@@ -22,7 +25,13 @@
 	const getError = (status) => {
 		const errCode = Object.keys(messages).find(key => key === status.toString());
 		return messages[errCode] || messages['unknown'];
-	}
+	};
+
+	const getCountryName = () => {
+		const { countryCode } = params;
+		const country = CountryService.getCountryByCode(countryCode);
+		return country.name;
+	};
 </script>
 
 <svelte:head>
@@ -36,6 +45,7 @@
 		</div>
 		<div class="text-xl md:text-2xl text-blue-light-300">
 			{getError(status)}
+			{getCountryName()}
 		</div>
 	</div>
 </div>
